8354c2602bacacda82a93c6ba0efd5e10c02173d
jgjxzqw <jgjxzqw@gmail.com>
2020-09-04 17:15:05 +0000

    could run code with simple feature engineer

--- code/main.py
+++ code/main.py
@@ -478,7 +478,7 @@ def kfold_lightgbm(df, num_folds, stratified=False, debug=False):
         params = {
             'objective': 'binary',
             'boosting_type': 'gbdt',
-            'nthread': 4,
+            'nthread': 8,
             'learning_rate': 0.02,  # 02,
             'num_leaves': 20,
             'colsample_bytree': 0.9497036,
@@ -541,39 +541,51 @@ def display_importances(feature_importance_df_):
 def main(debug=False):
     num_rows = 10000 if debug else None
     df = application_train_test(num_rows)
-    with timer("Process bureau and bureau_balance"):
-        bureau = bureau_and_balance(num_rows)
-        print("Bureau df shape:", bureau.shape)
-        df = df.join(bureau, how='left', on='SK_ID_CURR')
-        del bureau
-        gc.collect()
-    with timer("Process previous_applications"):
-        prev = previous_applications(num_rows)
-        print("Previous applications df shape:", prev.shape)
-        df = df.join(prev, how='left', on='SK_ID_CURR')
-        del prev
-        gc.collect()
-    with timer("Process POS-CASH balance"):
-        pos = pos_cash(num_rows)
-        print("Pos-cash balance df shape:", pos.shape)
-        df = df.join(pos, how='left', on='SK_ID_CURR')
-        del pos
-        gc.collect()
-    with timer("Process installments payments"):
-        ins = installments_payments(num_rows)
-        print("Installments payments df shape:", ins.shape)
-        df = df.join(ins, how='left', on='SK_ID_CURR')
-        del ins
-        gc.collect()
-    with timer("Process credit card balance"):
-        cc = credit_card_balance(num_rows)
-        print("Credit card balance df shape:", cc.shape)
-        df = df.join(cc, how='left', on='SK_ID_CURR')
-        del cc
-        gc.collect()
+    # with timer("Process bureau and bureau_balance"):
+    #     bureau = bureau_and_balance(num_rows)
+    #     print("Bureau df shape:", bureau.shape)
+    #     df = df.join(bureau, how='left', on='SK_ID_CURR')
+    #     del bureau
+    #     gc.collect()
+    # with timer("Process previous_applications"):
+    #     prev = previous_applications(num_rows)
+    #     print("Previous applications df shape:", prev.shape)
+    #     df = df.join(prev, how='left', on='SK_ID_CURR')
+    #     del prev
+    #     gc.collect()
+    # with timer("Process POS-CASH balance"):
+    #     pos = pos_cash(num_rows)
+    #     print("Pos-cash balance df shape:", pos.shape)
+    #     df = df.join(pos, how='left', on='SK_ID_CURR')
+    #     del pos
+    #     gc.collect()
+    # with timer("Process installments payments"):
+    #     ins = installments_payments(num_rows)
+    #     print("Installments payments df shape:", ins.shape)
+    #     df = df.join(ins, how='left', on='SK_ID_CURR')
+    #     del ins
+    #     gc.collect()
+    # with timer("Process credit card balance"):
+    #     cc = credit_card_balance(num_rows)
+    #     print("Credit card balance df shape:", cc.shape)
+    #     df = df.join(cc, how='left', on='SK_ID_CURR')
+    #     del cc
+    #     gc.collect()
     with timer("Run LightGBM with kfold"):
         print(df.shape)
-        df.drop(features_with_no_imp_at_least_twice, axis=1, inplace=True)
+
+        features_need_drop = [feature for feature in features_with_no_imp_at_least_twice if feature in df.columns]
+        df.drop(features_need_drop, axis=1, inplace=True)
+
+        for feature_name in df.columns:
+            if ('/' in feature_name or ':' in feature_name or ',' in feature_name or ' ' in feature_name):
+                x = feature_name.replace('/', '_')
+                x = x.replace(',', '_')
+                x = x.replace(':', '_')
+                x = x.replace(' ', '_')
+                df = df.rename(columns={feature_name: x})
+
+
         gc.collect()
         print(df.shape)
         feat_importance = kfold_lightgbm(df, num_folds=5, stratified=False, debug=debug)
